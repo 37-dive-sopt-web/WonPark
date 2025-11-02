1f1e9e5f4ef6b297600747ec13d5e5d7755f6114
wonpark1 <2023111927@dgu.ac.kr>
2025-11-03 05:34:54 +0900

    feat: board component 구현

--- week_3/homework/src/components/Board.jsx
+++ week_3/homework/src/components/Board.jsx
@@ -1,1 +1,21 @@
 // 보드 컴포넌트
+import Card from "./Card";
+
+export default function Board({ cards, cols, openIds, matched, onFlip }) {
+  return (
+    <div
+      className="grid gap-3"
+      style={{ gridTemplateColumns: `repeat(${cols}, 4rem)` }}
+    >
+      {cards.map((c) => (
+        <Card
+          key={c.id}
+          value={c.value}
+          open={openIds.includes(c.id)}
+          matched={matched.has(c.id)}
+          onClick={() => onFlip(c.id)}
+        />
+      ))}
+    </div>
+  );
+}
